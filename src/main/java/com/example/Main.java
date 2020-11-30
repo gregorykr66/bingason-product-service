3f4ac1b24e47211ad33c5d18d813139cad4c0cb1
GregoryKr <gregorykr66@gmail.com>
2020-11-30 12:47:10 +0100

    Add fetch product

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,36 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 public class Main {
 
-  @RequestMapping("/")
-  String index() {
-    return "Running";
-  }
+    @RequestMapping("/")
+    String index() {
+        return "Running";
+    }
 
-  public static void main(String[] args) throws Exception {
-    /*
-    // Listener services use:
-    Service.service(new Subscription[]{
-        new Subscription("user-history", "buy-product", (body, sender) -> {
-          System.out.println("user-history: buy-product");
-        }),
-        new Subscription("user-history", "login", (body, sender) -> {
-          System.out.println("user-history: login");
-          sender.send("buy-product", "Coffee");
-        })
-    });
-    */
+    public static void main(String[] args) throws Exception {
+        Service.service(new Subscription[]{
+                new Subscription("product", "fetch-product", (body, sender) -> {
+                    System.out.println("product: fetch-product");
+                    sender.send("display", "SESSION_ID,product,PRODUCT_ID,Coffee");
+                })
+        });
 
-    // Gateway service uses:
-    Subscription.Sender globalSender = Service.service(new Subscription[]{
-        new Subscription("display", "display", (body, sender) -> {
-          System.out.println("display: display");
-          System.out.println(body);
-          // Push message over socket to the user
-        })
-    });
-    globalSender.send("display", "Everything works!");
-
-    SpringApplication.run(Main.class, args);
-  }
+        SpringApplication.run(Main.class, args);
+    }
 
 }
